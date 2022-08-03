ad589f4b2944fd7822b73bb963b30724ecda935a
jsewill <jsewill@example.com>
2022-08-03 06:31:09 -0500

    Initial commit.

---
+++ generate.go
@@ -0,0 +1,14 @@
+/* Generate is a program for generating NFTs, and is meant to compliment http://github.com/Jsewill/chia/nft/mint */
+package main
+
+/*import (
+	nftstorage "github.com/nftstorage/go-client"
+	_ "github.com/stretchr/testify/assert"
+	_ "golang.org/x/net/context"
+	_ "golang.org/x/oauth2"
+)
+
+// Just something to keep go happy about the import until we decide whether or not to use it.
+func Upload() {
+	nft := nftstorage.NewNFT()
+}*/
